a54a33111a40080e695a6b71cd6f03c5bef2ea4e
Valentin Sene <valentin.sene@epitech.eu>
2020-04-08 14:52:01 +0200

    + requirements.txt contains required modules * run.py added modules loadind try

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+try:
+    import flask
+    import flask_sqlalchemy
+except:
+    print("Failed to import required modules. Try to run:\npip install --no-cache-dir -r requirements.txt")
+
 from app import app
 
 app.run()
