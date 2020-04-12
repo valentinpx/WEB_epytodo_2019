a9376906631b9198fbff38f16c3b694bd970c843
Valentin Sene <valentin.sene@epitech.eu>
2020-04-12 22:11:36 +0200

    * views added templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from flask import request, session
+from flask import request, session, render_template
+import jinja2
 from app import app
 from .controller import *
 
-@app.route("/")
-def hello_world():
-    return ("<a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ?t=1\">Hello, World!</a>")
+# interactions with bdd
+
+@app.route('/', methods=['GET'])
+def view_index():
+    return render_template('index.html')
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -37,3 +40,35 @@ def add_task():
 @app.route("/user/task/del/<int:id>", methods=["POST"])
 def del_task(id):
     return (rm_task(session, id))
+
+# interactions with templates
+
+@app.route('/login', methods=['GET'])
+def view_login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET'])
+def view_register():
+    return render_template('register.html')
+
+
+@app.route('/home', methods=['GET'])
+def view_home():
+    if ("ID" in session):
+        return render_template('home.html')
+    return render_template('login-redirect.html')
+
+
+@app.route('/add', methods=['GET'])
+def view_add():
+    if ("ID" in session):
+        return render_template('add.html')
+    return render_template('login-redirect.html')
+
+
+@app.route('/edit/<id>', methods=['GET'])
+def view_edit(id):
+    if ("ID" in session):
+        return render_template('edit.html')
+    return render_template('login-redirect.html')
